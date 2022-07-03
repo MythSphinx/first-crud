73591d27236ff2919f4fa67779efa609a6dc2c83
MythSphinx <alexjr968@outlook.com>
2022-07-03 07:46:05 -0300

    Database created, tested requests

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,57 @@
 import "./App.css";
+import React, { useState, useEffect } from "react";
+import Axios from "axios";
 
 function App() {
-  return (
-    <div className="form">
-      <h1>CRUD APPLICATION</h1>
-      <div>
-        <label>Album:</label>
-        <input type="text" name="albumName" />
-        <label>Band:</label>
-        <input Atype="text" name="bandName" />
+  const [album, setAlbum] = useState("");
+  const [band, setBand] = useState("");
+  const [albumList, setAlbumList] = useState([]);
 
-        <button>Submit</button>
+  useEffect(() => {
+    Axios.get("http://localhost:3001/api/get").then((response) => {
+      setAlbumList(response.data);
+    });
+  }, []);
+
+  const submitBand = () => {
+    Axios.post("http://localhost:3001/api/insert", {
+      albumName: album,
+      bandName: band,
+    }).then(() => {
+      alert.apply("succesful insert");
+    });
+  };
+
+  return (
+    <div className="App">
+      <h1>CRUD APPLICATION</h1>
+
+      <div className="form">
+        <label>Album:</label>
+        <input
+          type="text"
+          name="album"
+          onChange={(e) => {
+            setAlbum(e.target.value);
+          }}
+        />
+        <label>Band:</label>
+        <input
+          type="text"
+          name="band"
+          onChange={(e) => {
+            setBand(e.target.value);
+          }}
+        />
+
+        <button onClick={submitBand}>Submit</button>
+        {albumList.map((val) => {
+          return (
+            <h1>
+              Album Name: {val.albumname} | Band Name: {val.bandname}
+            </h1>
+          );
+        })}
       </div>
     </div>
   );
--- server/index.js
+++ server/index.js
@@ -1,19 +1,34 @@
 const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
 const app = express();
 const mysql = require("mysql");
 
-const db = mysql.createConnection({
+const db = mysql.createPool({
   host: "localhost",
   user: "root",
   password: "password",
   database: "cruddatabase",
 });
 
-app.get("/", (req, res) => {
-  const sqlInsert =
-    "INSERT INTO cruddb.albums (albumname, bandname) VALUES ('parish', 'seether');";
-  db.query(sqlInsert, (err, result) => {
-    res.send("helloworld");
+app.use(cors());
+app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/api/get", (req, res) => {
+  const sqlSelect = "SELECT * FROM  albums";
+  db.query(sqlSelect, (err, result) => {
+    res.send(result);
+  });
+});
+
+app.post("/api/insert", (req, res) => {
+  const albumName = req.body.albumName;
+  const bandName = req.body.bandName;
+
+  const sqlInsert = "INSERT INTO albums (albumName, bandName) VALUES (?,?)";
+  db.query(sqlInsert, [albumName, bandName], (err, result) => {
+    console.log(result);
   });
 });
 
